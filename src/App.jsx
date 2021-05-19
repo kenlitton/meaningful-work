41cdc84a16abd39a7b8417171321671105b62d25
Ken Litton <KennethCLitton@gmail.com>
2021-05-19 09:21:29 -0500

    in progress of setting up webpack

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Motivated Individuals</h1>
+      // <section>
+      //   <ResponseContainer name='Motivated Individual' />
+      // </section>
+    );
+  }
+}
+
+class ResponseContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      responsesArr : [],
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>See what responses others have gotten</h3>
+        <Response name={this.props.name}/>
+      </div>
+    );
+  }
+}
+
+class Response extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div>
+        <h5>Placeholder for {this.props.name}</h5>
+        <button>Click me to see details</button>
+      </div>
+    );
+  }
+}
+
+// export default App;
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env', 
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  },
+
+}
